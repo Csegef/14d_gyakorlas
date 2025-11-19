8c7b31f3f7c8258a7ca81591135e9ed832ab9b4e
Fekete <feketecsegeistvan-40257@taszi.hu>
2025-11-19 10:46:43 +0100

    Recipe.cs valtoztatas

---
+++ DummyJsonRecipes/MVVM/Model/Recipe.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DummyJsonRecipes.MVVM.Model
+{
+        public class Recipe
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+            public string[] ingredients { get; set; }
+            public string[] instructions { get; set; }
+            public int prepTimeMinutes { get; set; }
+            public int cookTimeMinutes { get; set; }
+            public int servings { get; set; }
+            public string difficulty { get; set; }
+            public string cuisine { get; set; }
+            public int caloriesPerServing { get; set; }
+            public string[] tags { get; set; }
+            public int userId { get; set; }
+            public string image { get; set; }
+            public float rating { get; set; }
+            public int reviewCount { get; set; }
+            public string[] mealType { get; set; }
+        }  
+}
