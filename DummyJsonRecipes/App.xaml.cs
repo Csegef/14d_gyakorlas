64fe88e7ca7df9c1c76c5af61ccbe3959a5f66e3
Fekete <feketecsegeistvan-40257@taszi.hu>
2025-11-19 10:08:17 +0100

    DummyJsonRecipes kezdés

---
+++ DummyJsonRecipes/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DummyJsonRecipes
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ DummyJsonRecipes/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DummyJsonRecipes
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
